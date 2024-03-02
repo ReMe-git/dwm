d7c1d12dd3da267fc073492c3b5ad4843a54e4ba
reme <reme@mail.reme.club>
2024-03-02 16:04:12 +0800

    config font

--- config.h
+++ config.h
@@ -5,8 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { ":size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "InconsolataGo Nerd Font:size=20 WenQuanYi Micro Hei:size=20" };
 static const char col_gray1[]       = "#1a1b26";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
